129d239f728d93548173142e5c7bf06d1f7b95c8
BGuney <beytullahguney@gmail.com>
2020-03-30 00:09:38 +0300

    Add project files.

---
+++ Multithreading/Functions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+
+namespace Multithreading
+{
+    public class Functions
+    {
+        /// <summary>
+        /// Method first
+        /// </summary>
+        public static void Function01()
+        {
+            int loopCount = 2000;
+
+            MethodBase.GetCurrentMethod();
+            for (int i = 0; i < loopCount; i++)
+            {
+                Console.WriteLine("{0} is {1}", MethodBase.GetCurrentMethod().ToString(), i);
+
+                if (i % 2 == 0)
+                {
+                    Thread.Sleep(2);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Second Method
+        /// </summary>
+        public static void Function02()
+        {
+            int loopCount = 2000;
+            for (int i = 0; i < loopCount; i++)
+            {
+                Console.WriteLine("{0} is {1}", MethodBase.GetCurrentMethod().ToString(), i);
+
+                if (i % 3 == 0)
+                {
+                    Thread.Sleep(2);
+                }
+            }
+        }
+    }
+}
---
+++ Multithreading/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace Multithreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(Functions.Function01);
+            Thread thread2 = new Thread(Functions.Function02);
+            thread1.Start();
+            thread2.Start();
+            Console.ReadLine();
+        }
+    }
+}
